bea42c8449d58b0cea5b84396fb814fbaab60930
pancho horrillo <pancho@pancho.name>
2025-10-08 12:30:52 +0200

    feat(traits/fish): tweak less, bat, man settings

--- modules/traits/development/man/hm/default.nix
+++ modules/traits/development/man/hm/default.nix
@@ -24,10 +24,14 @@ in
     manual.json.enable = true;
     manual.manpages.enable = true;
 
-    home.packages = [
-      pkgs.man-pages
-      pkgs.man-pages-posix
-    ];
+    home = {
+      sessionVariables.MANROFFOPT = "-P-i";
+      packages = [
+        pkgs.groff
+        pkgs.man-pages
+        pkgs.man-pages-posix
+      ];
+    };
 
     programs.man = {
       enable = true;
