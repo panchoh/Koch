bad813a9048c8da25cc54482e4dcb978ac1e5672
pancho horrillo <pancho@pancho.name>
2025-10-21 19:50:02 +0200

    refactor(nixos-configurations): leverage lib, mkNixosSystem more direct

--- modules/nixos-configurations.nix
+++ modules/nixos-configurations.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   self,
   inputs,
   ...
@@ -6,19 +7,19 @@
 {
   flake.nixosConfigurations =
     let
-      inherit (inputs.nixpkgs.lib.attrsets) nameValuePair;
-      inherit (inputs.nixpkgs.lib) nixosSystem;
-
-      mkSystem =
+      mkNixosSystem =
         box:
-        nameValuePair box.hostName (nixosSystem {
-          specialArgs = { inherit self inputs box; };
+        (lib.nixosSystem {
+          specialArgs = {
+            inherit self inputs box;
+          };
           modules = [
             self.nixosModules.default
           ]
           ++ box.extraModules;
         });
-
     in
-    self.lib.boxen |> map mkSystem |> builtins.listToAttrs;
+    self.lib.boxen
+    |> map (box: lib.nameValuePair box.hostName (mkNixosSystem box))
+    |> builtins.listToAttrs;
 }
