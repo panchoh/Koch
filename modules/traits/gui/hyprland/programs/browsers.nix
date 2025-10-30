ee84f7cf7e639fadc3b4eb80875c6a6a4ecea3f7
pancho horrillo <pancho@pancho.name>
2025-10-30 07:58:21 +0100

    feat(traits/hyprland): drop Chrome workspace and reinstante binding

--- modules/traits/gui/hyprland/programs/browsers.nix
+++ modules/traits/gui/hyprland/programs/browsers.nix
@@ -13,9 +13,7 @@
         wayland.windowManager.hyprland.settings = {
           bind = [
             "SUPER,       slash, exec, chromium"
-          ];
-          workspace = [
-            "10, defaultName:Chrome,   on-created-empty: google-chrome-stable"
+            "SUPER SHIFT, slash, exec, google-chrome-stable"
           ];
         };
       };
