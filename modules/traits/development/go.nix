4df2fe34dd956e194f3efc847bef37c8bf24c0d1
pancho horrillo <pancho@pancho.name>
2026-04-15 22:23:26 +0200

    fix(traits/go): sidestep file conflict in gopls and gotools
    See https://github.com/NixOS/nixpkgs/issues/509480

--- modules/traits/development/go.nix
+++ modules/traits/development/go.nix
@@ -27,23 +27,29 @@
           };
         };
 
-        home = {
-          packages = [
-            pkgs.capslock
-            pkgs.go-task
-            pkgs.gotools
-            pkgs.go-tools
-            pkgs.gopls
-            pkgs.gofumpt
-            pkgs.gomodifytags
-            pkgs.gotests
-            pkgs.gore
-            pkgs.godef
-            pkgs.delve
-            pkgs.gdlv
-            pkgs.golangci-lint
-          ];
-        };
+        home.packages = [
+          pkgs.capslock
+          pkgs.go-task
+          pkgs.gotools
+          pkgs.go-tools
+          # REVIEW: when NixOS/nixpkgs#509480 gets addressed
+          # pkgs.gopls
+          (pkgs.symlinkJoin {
+            name = "gopls-sans-modernize";
+            paths = [ pkgs.gopls ];
+            postBuild = ''
+              rm -f "$out/bin/modernize"
+            '';
+          })
+          pkgs.gofumpt
+          pkgs.gomodifytags
+          pkgs.gotests
+          pkgs.gore
+          pkgs.godef
+          pkgs.delve
+          pkgs.gdlv
+          pkgs.golangci-lint
+        ];
       };
     };
 }
