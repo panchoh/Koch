34cf59121754eb871ab9567a2830792d54eb932d
pancho horrillo <pancho@pancho.name>
2026-05-06 09:18:08 +0200

    chore(traits/mpd): update to track upstream changes

--- modules/traits/services/mpd.nix
+++ modules/traits/services/mpd.nix
@@ -20,20 +20,22 @@
         config = lib.mkIf cfg.enable {
           services.mpd = {
             enable = true;
-            musicDirectory = "/srv/media/audio";
             startWhenNeeded = true;
-            extraConfig = ''
-              audio_output {
-                      type            "alsa"
-                      name            "MM-1"
-                      device          "hw:1,0"        # optional
-                      #format         "44100:16:2"    # optional
-                      format          "48000:16:2"    # optional
-                      mixer_device    "hw:1"          # optional
-                      mixer_control   "PCM"           # optional
-                      mixer_index     "0"             # optional
-              }
-            '';
+            settings = {
+              music_directory = "/srv/media/audio";
+              audio_output = [
+                {
+                  type = "alsa";
+                  name = "MM-1";
+                  device = "hw:1,0"; # optional
+                  #format = "44100:16:2"; # optional
+                  format = "48000:16:2"; # optional
+                  mixer_device = "hw:1"; # optional
+                  mixer_control = "PCM"; # optional
+                  mixer_index = "0"; # optional
+                }
+              ];
+            };
           };
         };
       };
