1ef544a06d6ce8361d87c1e26b01e0f32b3dcea3
pancho horrillo <pancho@pancho.name>
2026-06-05 08:04:34 +0200

    tweak(traits/kmscon): mode for 5K monitor and sane defaults

--- modules/traits/services/kmscon.nix
+++ modules/traits/services/kmscon.nix
@@ -21,7 +21,18 @@
           getty.autologinUser = box.userName or "alice";
           kmscon = {
             enable = true;
-            config.hwaccel = true;
+            config = {
+              hwaccel = true;
+              dpms-timeout = 300;
+              grab-reboot = "<Ctrl><Alt>Delete";
+              bell = true;
+            }
+            // lib.optionalAttrs (box.hostName == "oxygen") {
+              use-original-mode = false;
+              mode = "5120x2160";
+              font-size = lib.mkForce 30;
+            };
+
             useXkbConfig = true;
           };
         };
