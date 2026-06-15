f53f2d0ca8b4e6fc38ca9549ad33d92447b03c82
pancho horrillo <pancho@pancho.name>
2026-06-15 09:37:38 +0200

    feat(traits/hyprland): config for xdph
    xdg-desktop-portal-hyprland rivals with PricewaterhouseCoopers…

--- modules/traits/gui/hyprland/default.nix
+++ modules/traits/gui/hyprland/default.nix
@@ -40,9 +40,17 @@
           };
         };
         config = lib.mkIf cfg.enable {
-          home.packages = [
-            pkgs.d-spy
-          ];
+          home = {
+            packages = [
+              pkgs.d-spy
+            ];
+            # https://wiki.hypr.land/Hypr-Ecosystem/xdg-desktop-portal-hyprland/
+            file."${config.xdg.configHome}/hypr/xdph.conf".text = ''
+              screencopy {
+                  allow_token_by_default = true
+              }
+            '';
+          };
           wayland.windowManager.hyprland = {
             # https://wiki.hypr.land/Nix/Hyprland-on-Home-Manager/#using-the-home-manager-module-with-nixos
             enable = true;
