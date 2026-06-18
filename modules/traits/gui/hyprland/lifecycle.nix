b68bc86a5dd34e036306d4df63fea2ef119cbb98
pancho horrillo <pancho@pancho.name>
2026-06-18 08:38:07 +0200

    feat(traits/hyprland): show notification upon hyprctl kill

--- modules/traits/gui/hyprland/lifecycle.nix
+++ modules/traits/gui/hyprland/lifecycle.nix
@@ -10,24 +10,41 @@
     in
     {
       config = lib.mkIf cfg.enable {
-        wayland.windowManager.hyprland.settings.bind =
-          {
-            # TODO: replace bare exit() call with hyprshutdown
-            # https://wiki.hypr.land/Hypr-Ecosystem/hyprshutdown/
-            "CONTROL + ALT + BackSpace" = "hl.dsp.exit()";
-            "SUPER + Q" = "hl.dsp.force_renderer_reload()";
-            "SUPER + X" = "hl.dsp.window.close()";
-            "SUPER + SHIFT + X" = "hl.dsp.window.kill()";
-            "SUPER + CONTROL + X" = ''hl.dsp.exec_cmd("hyprctl kill")'';
-          }
-          |> lib.mapAttrsToList (
-            keys: dispatcher: {
+        wayland.windowManager.hyprland = {
+          settings = {
+            on = {
               _args = [
-                keys
-                (lib.generators.mkLuaInline dispatcher)
+                "window.kill"
+                (lib.generators.mkLuaInline ''
+                  function(w)
+                    hl.notification.create({
+                      text = "Headshot! (via hyprctl kill) : " .. w.class .. "." .. w.title,
+                      timeout = 5000,
+                      icon = "INFO" })
+                  end
+                '')
               ];
-            }
-          );
+            };
+            bind =
+              {
+                # TODO: replace bare exit() call with hyprshutdown
+                # https://wiki.hypr.land/Hypr-Ecosystem/hyprshutdown/
+                "CONTROL + ALT + BackSpace" = "hl.dsp.exit()";
+                "SUPER + Q" = "hl.dsp.force_renderer_reload()";
+                "SUPER + X" = "hl.dsp.window.close()";
+                "SUPER + SHIFT + X" = "hl.dsp.window.kill()";
+                "SUPER + CONTROL + X" = ''hl.dsp.exec_cmd("hyprctl kill")'';
+              }
+              |> lib.mapAttrsToList (
+                keys: dispatcher: {
+                  _args = [
+                    keys
+                    (lib.generators.mkLuaInline dispatcher)
+                  ];
+                }
+              );
+          };
+        };
       };
     };
 }
