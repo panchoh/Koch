e97dfec5a0bbbf55ed692b4c0e9139ac4eea357e
pancho horrillo <pancho@pancho.name>
2026-06-23 22:50:17 +0200

    fix(traits/hyprland): tweak passwd window on ff

--- modules/traits/gui/hyprland/programs/browsers.nix
+++ modules/traits/gui/hyprland/programs/browsers.nix
@@ -11,6 +11,15 @@
     {
       config = lib.mkIf cfg.enable {
         wayland.windowManager.hyprland.settings = {
+          window_rule = [
+            {
+              match.initial_title = "Password Required - Mozilla Firefox";
+              no_anim = true;
+              xray = true;
+              dim_around = true;
+              stay_focused = true;
+            }
+          ];
           bind =
             {
               "SUPER + slash" = "chromium";
