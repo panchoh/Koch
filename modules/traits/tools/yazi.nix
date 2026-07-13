04ffb58c892a33d520637b8e290ab64edea42362
pancho horrillo <pancho@pancho.name>
2026-07-13 08:41:40 +0200

    refactor(traits/yazi): move to tools/

--- modules/traits/tools/yazi.nix
+++ modules/traits/tools/yazi.nix
@@ -3,12 +3,19 @@
     {
       config,
       lib,
+      box ? null,
       ...
     }:
     let
-      cfg = config.traits.hm.fish;
+      cfg = config.traits.hm.yazi;
     in
     {
+      options.traits.hm.yazi = {
+        enable = lib.mkEnableOption "yazi" // {
+          default = box.isStation or false;
+        };
+      };
+
       config = lib.mkIf cfg.enable {
         programs.yazi = {
           enable = true;
