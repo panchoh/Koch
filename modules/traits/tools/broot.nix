ba390bcc58ea1fe0cffaf19557bb10c69e80de73
pancho horrillo <pancho@pancho.name>
2026-07-13 08:39:56 +0200

    refactor(traits/broot): move to tools/

--- modules/traits/tools/broot.nix
+++ modules/traits/tools/broot.nix
@@ -3,12 +3,19 @@
     {
       config,
       lib,
+      box ? null,
       ...
     }:
     let
-      cfg = config.traits.hm.fish;
+      cfg = config.traits.hm.broot;
     in
     {
+      options.traits.hm.broot = {
+        enable = lib.mkEnableOption "broot" // {
+          default = box.isStation or false;
+        };
+      };
+
       config = lib.mkIf cfg.enable {
         # https://dystroy.org/broot/
         programs.broot = {
