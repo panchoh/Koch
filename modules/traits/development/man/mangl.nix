76d38d014fd64db2f1b2f9f862a36e4ac9e62619
pancho horrillo <pancho@pancho.name>
2026-07-13 11:42:56 +0200

    fix(traits/mangl): enable conditionally

--- modules/traits/development/man/mangl.nix
+++ modules/traits/development/man/mangl.nix
@@ -4,14 +4,23 @@
       config,
       lib,
       pkgs,
+      nixosConfig,
+      box ? null,
       ...
     }:
     let
       cfg = config.traits.hm.man;
     in
     {
+      options.traits.hm.mangl = {
+        enable = lib.mkEnableOption "mangl" // {
+          default = box.isStation or false;
+        };
+      };
+
       config = lib.mkIf cfg.enable {
-        home.packages = [
+        home.packages = lib.optionals nixosConfig.documentation.man.man-db.enable [
+          # BUG: mangl is based on mandoc lib, but misses env: ‘manpath’
           pkgs.mangl # https://github.com/zigalenarcic/mangl
         ];
 
