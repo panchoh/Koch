8f8566c796709db3f48b526f14f5bce16953e410
pancho horrillo <pancho@pancho.name>
2026-07-13 07:42:32 +0200

    refactor(traits/tealdeer): move to tools/

--- modules/traits/tools/tealdeer.nix
+++ modules/traits/tools/tealdeer.nix
@@ -3,12 +3,19 @@
     {
       config,
       lib,
+      box ? null,
       ...
     }:
     let
-      cfg = config.traits.hm.fish;
+      cfg = config.traits.hm.tealdeer;
     in
     {
+      options.traits.hm.tealdeer = {
+        enable = lib.mkEnableOption "tealdeer" // {
+          default = box.isStation or false;
+        };
+      };
+
       config = lib.mkIf cfg.enable {
         programs.tealdeer = {
           enable = true;
