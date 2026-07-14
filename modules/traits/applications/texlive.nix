070afbd383cc66fd28289c776ca0f22d7cea93a5
pancho horrillo <pancho@pancho.name>
2026-07-14 08:21:15 +0200

    chore(traits/texlive): disable until fix available
    texlive.combine is deprecated, and it emits a warning, which is fatal
    for us.

--- modules/traits/applications/texlive.nix
+++ modules/traits/applications/texlive.nix
@@ -17,8 +17,21 @@
       };
 
       config = lib.mkIf cfg.enable {
+        # https://nixos.org/manual/nixpkgs/unstable/#sec-language-texlive-user-guide
+        #
+        # FIXME:
+        # removal: texlive.combine is deprecated and will be removed from
+        # Nixpkgs 27.05. Please switch to texliveSmall.withPackages. See
+        # https://nixos.org/manual/nixpkgs/stable/#sec-language-texlive-user-guide. See
+        # https://nixos.org/manual/nixpkgs/unstable#sec-problems
+        #
+        # See:
+        # pkgs.texlive.scheme-*
+        # pkgs.texlive.schemes.*
+        #
+        # REVIEW: Disabling until this can be reworked on Home Manager
         programs.texlive = {
-          enable = true;
+          enable = false;
           extraPackages = tpkgs: {
             inherit (tpkgs) scheme-full;
           };
