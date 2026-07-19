813e7b554ec236d34dc7b3899530242e7b15fb48
pancho horrillo <pancho@pancho.name>
2026-07-19 10:17:40 +0200

    tweak(traits/lsd): handle abbrs
    The default abbreviations exposed by programs.lsd is not very human-friendly.

--- modules/traits/tools/lsd.nix
+++ modules/traits/tools/lsd.nix
@@ -17,9 +17,23 @@
       };
 
       config = lib.mkIf cfg.enable {
+        programs.fish.shellAbbrs = {
+          l = "lsd --long --almost-all";
+          ls = "lsd";
+          lso = "lsd --oneline";
+          ll = "lsd --long";
+          la = "lsd --almost-all";
+          lao = "lsd --almost-all --one-line";
+          lt = "lsd --tree";
+          lla = "lsd --long --almost-all";
+          llt = "lsd --long --tree";
+          llat = "lsd --long --almost-all --tree";
+        };
+
         programs.lsd = {
           enable = true;
           enableBashIntegration = false;
+          enableFishIntegration = false;
           enableZshIntegration = false;
           settings = {
             date = "relative";
