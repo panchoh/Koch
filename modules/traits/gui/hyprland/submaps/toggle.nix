aa8e2dd09800089bea436cbb86a91906db26d334
pancho horrillo <pancho@pancho.name>
2026-07-24 23:46:11 +0200

    feat(traits/hyprland): keybind toggle
    By entering the 'clean' submap, all keybinds will be available to the
    active client.  For instance, wev.

---
+++ modules/traits/gui/hyprland/submaps/toggle.nix
@@ -0,0 +1,38 @@
+{
+  flake.homeModules.default =
+    {
+      config,
+      lib,
+      ...
+    }:
+    let
+      cfg = config.traits.hm.hyprland;
+    in
+    {
+      config =
+        let
+          # https://wiki.hypr.land/Configuring/Basics/Binds/#disabling-keybinds-with-one-master-keybind
+          keys = "SUPER + CONTROL + ALT + Escape";
+        in
+        lib.mkIf cfg.enable {
+          wayland.windowManager.hyprland = {
+            settings.bind = [
+              {
+                _args = [
+                  keys
+                  (lib.generators.mkLuaInline ''hl.dsp.submap("clean")'')
+                ];
+              }
+            ];
+            submaps.clean.settings.bind = [
+              {
+                _args = [
+                  keys
+                  (lib.generators.mkLuaInline ''hl.dsp.submap("reset")'')
+                ];
+              }
+            ];
+          };
+        };
+    };
+}
