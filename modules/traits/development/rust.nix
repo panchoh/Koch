b56895ad59663cddd6a06bb5d89e926e4802f30d
pancho horrillo <pancho@pancho.name>
2026-07-26 11:26:42 +0200

    feat(traits/rust): init

---
+++ modules/traits/development/rust.nix
@@ -0,0 +1,29 @@
+{
+  flake.homeModules.default =
+    {
+      config,
+      lib,
+      pkgs,
+      box ? null,
+      ...
+    }:
+    let
+      cfg = config.traits.hm.rust;
+    in
+    {
+      options.traits.hm.rust = {
+        enable = lib.mkEnableOption "Rust" // {
+          default = box.isStation or false;
+        };
+      };
+
+      config = lib.mkIf cfg.enable {
+
+        home.packages = [
+          pkgs.cargo
+          pkgs.rustc
+          pkgs.rust-analyzer
+        ];
+      };
+    };
+}
