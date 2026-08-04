cac2c5d2dcc03ee01ef379300aa20cc4d11bd570
pancho horrillo <pancho@pancho.name>
2026-08-04 14:11:32 +0200

    feat(traits/hypr): lid: don’t suspend if clamshell

--- modules/traits/gui/hyprland/lid.nix
+++ modules/traits/gui/hyprland/lid.nix
@@ -1,38 +1,60 @@
 {
-  flake.homeModules.default =
-    {
-      config,
-      lib,
-      ...
-    }:
+  flake = {
+    nixosModules.default =
+      {
+        config,
+        lib,
+        ...
+      }:
 
-    let
-      cfg = config.traits.hm.hyprland;
-    in
-    {
-      config = lib.mkIf cfg.enable {
-        # Lid management
-        # https://www.reddit.com/r/hyprland/comments/1tbugwn/disabling_monitor_using_keybinds_in_055/
-        # https://wiki.hypr.land/Configuring/Basics/Binds/#switches
-        # https://github.com/hyprwm/Hyprland/discussions/11093
-        wayland.windowManager.hyprland.settings.bind =
-          {
-            "switch:on:Lid Switch" = "true";
-            "switch:off:Lid Switch" = "false";
-          }
-          |> lib.mapAttrsToList (
-            keys: disabled: {
-              _args = [
-                keys
-                (lib.generators.mkLuaInline ''
-                  function()
-                    hl.monitor({ output = "eDP-1", disabled = ${disabled} })
-                  end
-                '')
-                { locked = true; }
-              ];
-            }
-          );
+      let
+        cfg = config.traits.os.hyprland;
+      in
+      {
+        config = lib.mkIf cfg.enable {
+          services.logind.settings.Login = {
+            # See logind.conf(5)
+            # HandleLidSwitchDocked = "ignore"; # default value
+            HandleLidSwitchExternalPower = "ignore";
+          };
+        };
       };
-    };
+
+    homeModules.default =
+      {
+        config,
+        lib,
+        ...
+      }:
+
+      let
+        cfg = config.traits.hm.hyprland;
+      in
+      {
+        config = lib.mkIf cfg.enable {
+          # Lid management
+          # https://www.reddit.com/r/hyprland/comments/1tbugwn/disabling_monitor_using_keybinds_in_055/
+          # https://wiki.hypr.land/Configuring/Basics/Binds/#switches
+          # https://github.com/hyprwm/Hyprland/discussions/11093
+          wayland.windowManager.hyprland.settings.bind =
+            {
+              "switch:on:Lid Switch" = "true";
+              "switch:off:Lid Switch" = "false";
+            }
+            |> lib.mapAttrsToList (
+              keys: disabled: {
+                _args = [
+                  keys
+                  (lib.generators.mkLuaInline ''
+                    function()
+                      hl.monitor({ output = "eDP-1", disabled = ${disabled} })
+                    end
+                  '')
+                  { locked = true; }
+                ];
+              }
+            );
+        };
+      };
+  };
 }
