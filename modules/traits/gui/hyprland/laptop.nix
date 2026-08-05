c117ceb7f63daeb9c746e215cef83e9c964fe1cf
pancho horrillo <pancho@pancho.name>
2026-08-05 11:26:27 +0200

    tweak(traits/hyprland): laptop: coding ws issues

--- modules/traits/gui/hyprland/laptop.nix
+++ modules/traits/gui/hyprland/laptop.nix
@@ -12,21 +12,23 @@
     in
     {
       config = lib.mkIf cfg.enable {
-        wayland.windowManager.hyprland.settings.workspace_rule = [
+        wayland.windowManager.hyprland.settings.workspace_rule = lib.optionals (box.isLaptop or false) [
           {
             workspace = "name:coding";
             monitor = "eDP-1";
-            default = !(box.hasWideDisplay or true);
+            default = true;
             layout = "monocle";
-            persistent = (box.isLaptop or false) && !(box.hasWideDisplay or true);
+            persistent = true;
             gaps_in = 0;
             gaps_out = 0;
             no_border = true;
             no_rounding = true;
             decorate = false;
-            on_created_empty = ''
-              hyprctl monitors -j | jq --raw-output --exit-status 'any(.[]; .name | test("^eDP-[0-9]$"))' > /dev/null && doom-emacs
-            '';
+            on_created_empty =
+              if (!box.hasExternalMonitor or true) then
+                "doom-emacs"
+              else
+                ''hyprctl monitors -j | jq --raw-output --exit-status 'any(.[]; .name | test("^DP-[0-9]$"))' > /dev/null || doom-emacs'';
           }
         ];
       };
