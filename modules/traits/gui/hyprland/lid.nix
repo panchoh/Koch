88e46deada18ac5bee9a9d80c0050c47d0c2b514
pancho horrillo <pancho@pancho.name>
2026-08-11 20:30:33 +0200

    feat(traits/hyprland): laptop: internal monitor
    - Disable when external monitor is plugged.
    - Enable when external monitor is unplugged.

--- modules/traits/gui/hyprland/lid.nix
+++ modules/traits/gui/hyprland/lid.nix
@@ -24,6 +24,7 @@
       {
         config,
         lib,
+        box ? null,
         ...
       }:
 
@@ -36,24 +37,42 @@
           # https://www.reddit.com/r/hyprland/comments/1tbugwn/disabling_monitor_using_keybinds_in_055/
           # https://wiki.hypr.land/Configuring/Basics/Binds/#switches
           # https://github.com/hyprwm/Hyprland/discussions/11093
-          wayland.windowManager.hyprland.settings.bind =
-            {
-              "switch:on:Lid Switch" = "true";
-              "switch:off:Lid Switch" = "false";
-            }
-            |> lib.mapAttrsToList (
-              keys: disabled: {
-                _args = [
-                  keys
-                  (lib.generators.mkLuaInline ''
-                    function()
-                      hl.monitor({ output = "eDP-1", disabled = ${disabled} })
-                    end
-                  '')
-                  { locked = true; }
-                ];
+          wayland.windowManager.hyprland = {
+
+            settings.bind =
+              {
+                "switch:on:Lid Switch" = "true";
+                "switch:off:Lid Switch" = "false";
               }
-            );
+              |> lib.mapAttrsToList (
+                keys: disabled: {
+                  _args = [
+                    keys
+                    (lib.generators.mkLuaInline ''
+                      function()
+                        hl.monitor({ output = "eDP-1", disabled = ${disabled} })
+                      end
+                    '')
+                    { locked = true; }
+                  ];
+                }
+              );
+
+            extraConfig = lib.optionalString box.hasExternalMonitor or false ''
+              hl.on("monitor.layout_changed", function()
+                  local internal_enabled = hl.get_monitor("eDP-1")
+                  local monitors = hl.get_monitors()
+                  local should_disable = #monitors > 1 and monitors[1].name ~= "FALLBACK"
+                  local should_enable = monitors[1] and monitors[1].name == "FALLBACK"
+
+                  if internal_enabled and should_disable then
+                      hl.monitor({output = "eDP-1", disabled = true })
+                  elseif not internal_enabled and should_enable then
+                      hl.monitor({output = "eDP-1", disabled = false })
+                  end
+              end)
+            '';
+          };
         };
       };
   };
