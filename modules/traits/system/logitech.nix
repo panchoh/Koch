9d27d920b9323f66719840850f65d50d71e48a54
pancho horrillo <pancho@pancho.name>
2026-08-22 09:48:13 +0200

    tweak(traits/logitech): don’t allow g15daemon udev rules

--- modules/traits/system/logitech.nix
+++ modules/traits/system/logitech.nix
@@ -20,7 +20,14 @@
 
       config = lib.mkIf cfg.enable {
         programs.solaar.enable = true;
-        hardware.logitech.wireless.enable = true;
+        hardware.logitech = {
+
+          # prevent g15daemon udev rules from creeping in.
+          lcd.devices = [ ];
+
+          # for ltunify; see https://lekensteyn.nl/logitech-unifying.html
+          wireless.enable = true;
+        };
       };
     };
 }
