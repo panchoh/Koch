48b51792789622c33802b8a84dbc6c61782ce2da
pancho horrillo <pancho@pancho.name>
2026-08-28 08:30:45 +0200

    chore(traits/elm): disable until build failure fix

--- modules/traits/development/elm.nix
+++ modules/traits/development/elm.nix
@@ -29,14 +29,15 @@
       {
         config = lib.mkIf cfg.enable {
 
+          # REVIEW: re-enable when build failures are addressed
           home.packages = [
             pkgs.elmPackages.elm
-            pkgs.elmPackages.elm-analyse
-            pkgs.elmPackages.elm-doc-preview
+            # pkgs.elmPackages.elm-analyse
+            # pkgs.elmPackages.elm-doc-preview
             pkgs.elmPackages.elm-format
             pkgs.elmPackages.elm-git-install
-            pkgs.elmPackages.elm-json
-            pkgs.elmPackages.elm-language-server
+            # pkgs.elmPackages.elm-json
+            # pkgs.elmPackages.elm-language-server
             pkgs.elmPackages.elm-live
             pkgs.elmPackages.elm-review
             pkgs.elmPackages.elm-spa
