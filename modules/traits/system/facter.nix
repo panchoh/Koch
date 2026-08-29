39f33c25b76002b5e2e227d9d01af8bb2badb372
pancho horrillo <pancho@pancho.name>
2026-08-29 14:16:52 +0200

    style(traits/facter): pack flake

--- modules/traits/system/facter.nix
+++ modules/traits/system/facter.nix
@@ -1,52 +1,55 @@
 {
-  flake.nixosModules.default =
-    {
-      config,
-      lib,
-      box ? null,
-      ...
-    }:
+  flake = {
 
-    let
-      cfg = config.traits.facter;
-    in
-    {
-      options.traits.facter = {
-        enable = lib.mkEnableOption "NixOS Facter" // {
-          default = true;
+    nixosModules.default =
+      {
+        config,
+        lib,
+        box ? null,
+        ...
+      }:
+
+      let
+        cfg = config.traits.facter;
+      in
+      {
+        options.traits.facter = {
+          enable = lib.mkEnableOption "NixOS Facter" // {
+            default = true;
+          };
+        };
+
+        config = lib.mkIf cfg.enable {
+
+          # https://nixos.org/manual/nixos/stable/#module-hardware-facter
+          # https://search.nixos.org/options?query=facter
+          # https://nix-community.github.io/nixos-facter
+          # https://github.com/nix-community/nixos-facter
+          # nix run .#nixosConfigurations.silicon.config.hardware.facter.debug.nvd
+          # nix run .#nixosConfigurations.silicon.config.hardware.facter.debug.nix-diff
+          hardware.facter.reportPath = box.facter;
+
         };
       };
 
-      config = lib.mkIf cfg.enable {
+    homeModules.default =
+      {
+        nixosConfig,
+        lib,
+        pkgs,
+        ...
+      }:
 
-        # https://nixos.org/manual/nixos/stable/#module-hardware-facter
-        # https://search.nixos.org/options?query=facter
-        # https://nix-community.github.io/nixos-facter
-        # https://github.com/nix-community/nixos-facter
-        # nix run .#nixosConfigurations.silicon.config.hardware.facter.debug.nvd
-        # nix run .#nixosConfigurations.silicon.config.hardware.facter.debug.nix-diff
-        hardware.facter.reportPath = box.facter;
+      let
+        cfg = nixosConfig.traits.facter;
+      in
+      {
+        config = lib.mkIf cfg.enable {
 
+          home.packages = [
+            pkgs.nixos-facter
+          ];
+        };
       };
-    };
-
-  flake.homeModules.default =
-    {
-      nixosConfig,
-      lib,
-      pkgs,
-      ...
-    }:
-
-    let
-      cfg = nixosConfig.traits.facter;
-    in
-    {
-      config = lib.mkIf cfg.enable {
-
-        home.packages = [
-          pkgs.nixos-facter
-        ];
-      };
-    };
+  };
 }
