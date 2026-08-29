cb6f1d81fb7eb4ae5fab47b9a1b58ee44e41a27c
pancho horrillo <pancho@pancho.name>
2026-08-29 13:28:17 +0200

    feat(traits/nh): init

---
+++ modules/traits/system/nh.nix
@@ -0,0 +1,56 @@
+{
+  flake = {
+
+    nixosModules.default =
+      {
+        lib,
+        ...
+      }:
+
+      {
+        options.traits.nh = {
+          enable = lib.mkEnableOption "NH" // {
+            default = true;
+          };
+        };
+      };
+
+    homeModules.default =
+      {
+        config,
+        nixosConfig,
+        lib,
+        pkgs,
+        box ? null,
+        ...
+      }:
+
+      let
+        cfg = nixosConfig.traits.nh;
+      in
+      {
+        config = lib.mkIf cfg.enable {
+
+          home.packages = [
+            pkgs.dix
+            pkgs.nix-output-monitor
+          ];
+
+          home.sessionVariables.NH_SHOW_ACTIVATION_LOGS = true;
+
+          programs.nh = {
+
+            enable = nixosConfig.nix.enable && !nixosConfig.system.disableInstallerTools;
+
+            clean = {
+              enable = true;
+              dates = "daily";
+              extraArgs = "--keep 5 --keep-since 3d";
+            };
+
+            flake = "${config.xdg.userDirs.projects}/${box.githubUser}/${box.flakeRepoName}";
+          };
+        };
+      };
+  };
+}
