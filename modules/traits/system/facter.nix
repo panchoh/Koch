985ecff536e48c98448bc2544c6a40c84bcf8e78
pancho horrillo <pancho@pancho.name>
2026-08-30 13:50:03 +0200

    refactor(traits/facter): support facter harvesting

--- modules/traits/system/facter.nix
+++ modules/traits/system/facter.nix
@@ -1,55 +1,36 @@
 {
-  flake = {
+  flake.nixosModules.default =
+    {
+      config,
+      lib,
+      pkgs,
+      box ? null,
+      ...
+    }:
 
-    nixosModules.default =
-      {
-        config,
-        lib,
-        box ? null,
-        ...
-      }:
-
-      let
-        cfg = config.traits.facter;
-      in
-      {
-        options.traits.facter = {
-          enable = lib.mkEnableOption "NixOS Facter" // {
-            default = true;
-          };
-        };
-
-        config = lib.mkIf cfg.enable {
-
-          # https://nixos.org/manual/nixos/stable/#module-hardware-facter
-          # https://search.nixos.org/options?query=facter
-          # https://nix-community.github.io/nixos-facter
-          # https://github.com/nix-community/nixos-facter
-          # nix run .#nixosConfigurations.silicon.config.hardware.facter.debug.nvd
-          # nix run .#nixosConfigurations.silicon.config.hardware.facter.debug.nix-diff
-          hardware.facter.reportPath = box.facter;
-
+    let
+      cfg = config.traits.facter;
+    in
+    {
+      options.traits.facter = {
+        enable = lib.mkEnableOption "NixOS Facter" // {
+          default = true;
         };
       };
 
-    homeModules.default =
-      {
-        nixosConfig,
-        lib,
-        pkgs,
-        ...
-      }:
+      config = lib.mkIf cfg.enable {
 
-      let
-        cfg = nixosConfig.traits.facter;
-      in
-      {
-        config = lib.mkIf cfg.enable {
+        # https://clan.lol/blog/nixos-facter/
+        # https://nixos.org/manual/nixos/stable/#module-hardware-facter
+        # https://search.nixos.org/options?query=facter
+        # https://nix-community.github.io/nixos-facter
+        # https://github.com/nix-community/nixos-facter
+        # nix run .#nixosConfigurations.silicon.config.hardware.facter.debug.nvd
+        # nix run .#nixosConfigurations.silicon.config.hardware.facter.debug.nix-diff
+        hardware.facter.reportPath = box.facter;
 
-          home.packages = [
-            pkgs.nixos-facter
-          ];
-        };
+        environment.systemPackages = [ pkgs.nixos-facter ];
+
       };
-  };
+    };
 }
