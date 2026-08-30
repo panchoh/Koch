0610653f112bf61d44260e66c0a1b4828e2a4f4e
pancho horrillo <pancho@pancho.name>
2026-08-30 11:50:52 +0200

    feat(traits/mpv): support nvidia, plus some tweaks

--- modules/traits/gui/mpv.nix
+++ modules/traits/gui/mpv.nix
@@ -33,6 +33,7 @@
 
           home.packages = [
 
+            pkgs.drm_info
             pkgs.playerctl
 
             # for Emacs’ org-pomodoro
@@ -84,30 +85,36 @@
               # https://github.com/mpv-player/mpv/issues/12082#issuecomment-1666545541
               defaultProfiles = [ (if !(box.hasBeefyGPU or true) then "fast" else "high-quality") ];
 
-              config = {
-                fullscreen = true;
-                sub-auto = "fuzzy";
+              config =
+                let
+                  gpus = nixosConfig.hardware.facter.report.hardware.graphics_card or [ ];
+                  hasNvidia = builtins.any (gpu: (gpu.vendor.name or "") == "nVidia Corporation") gpus;
+                in
+                {
+                  fullscreen = true;
+                  sub-auto = "fuzzy";
 
-                gpu-context = "waylandvk";
-                hwdec = "vaapi";
+                  gpu-api = "vulkan";
+                  hwdec = if hasNvidia then "nvdec" else "vaapi";
+                  hwdec-codecs = if hasNvidia then "h264,hevc,vp8,vp9,vc1" else "all";
 
-                # https://github.com/mpv-player/mpv/issues/8981
-                # hdr-compute-peak = false;
+                  # https://github.com/mpv-player/mpv/issues/8981
+                  # hdr-compute-peak = false;
 
-                # https://github.com/mpv-player/mpv/issues/10972#issuecomment-1340100762
-                # vd-lavc-dr = false;
+                  # https://github.com/mpv-player/mpv/issues/10972#issuecomment-1340100762
+                  # vd-lavc-dr = false;
 
-                # drm-vrr-enabled = "auto";
+                  # drm-vrr-enabled = "auto";
 
-                # HDR
-                # https://github.com/mpv-player/mpv/pull/16512
-                # https://mpv.io/manual/master/#options-target-colorspace-hint
-                # target-colorspace-hint = "auto";
-                # https://mpv.io/manual/master/#options-target-colorspace-hint-mode
-                # https://wiki.hypr.land/Configuring/Basics/Variables/#render
-                # requires vo = "gpu-next"
-                # target-colorspace-hint-mode = "source"; # after mpv 0.40.0
-              };
+                  # HDR
+                  # https://github.com/mpv-player/mpv/pull/16512
+                  # https://mpv.io/manual/master/#options-target-colorspace-hint
+                  # target-colorspace-hint = "auto";
+                  # https://mpv.io/manual/master/#options-target-colorspace-hint-mode
+                  # https://wiki.hypr.land/Configuring/Basics/Variables/#render
+                  # requires vo = "gpu-next"
+                  # target-colorspace-hint-mode = "source"; # after mpv 0.40.0
+                };
 
               profiles = {
                 alsa-mm1 = {
