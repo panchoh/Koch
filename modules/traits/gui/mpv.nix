e50743f6db599c3e03df4ff09d96eddcd0849bcc
pancho horrillo <pancho@pancho.name>
2026-08-31 08:09:17 +0200

    tweak(traits/mpv): cleanup

--- modules/traits/gui/mpv.nix
+++ modules/traits/gui/mpv.nix
@@ -93,27 +93,12 @@
                 {
                   fullscreen = true;
                   sub-auto = "fuzzy";
-
                   gpu-api = "vulkan";
-                  hwdec = if hasNvidia then "nvdec" else "vaapi";
-                  hwdec-codecs = if hasNvidia then "h264,hevc,vp8,vp9,vc1" else "all";
-
-                  # https://github.com/mpv-player/mpv/issues/8981
-                  # hdr-compute-peak = false;
-
-                  # https://github.com/mpv-player/mpv/issues/10972#issuecomment-1340100762
-                  # vd-lavc-dr = false;
-
-                  # drm-vrr-enabled = "auto";
-
-                  # HDR
-                  # https://github.com/mpv-player/mpv/pull/16512
-                  # https://mpv.io/manual/master/#options-target-colorspace-hint
-                  # target-colorspace-hint = "auto";
-                  # https://mpv.io/manual/master/#options-target-colorspace-hint-mode
-                  # https://wiki.hypr.land/Configuring/Basics/Variables/#render
-                  # requires vo = "gpu-next"
-                  # target-colorspace-hint-mode = "source"; # after mpv 0.40.0
+                  hwdec = "vaapi";
+                }
+                // lib.optionalAttrs hasNvidia {
+                  hwdec = "nvdec";
+                  hwdec-codecs = "h264,hevc,vp8,vp9,vc1";
                 };
 
               profiles = {
